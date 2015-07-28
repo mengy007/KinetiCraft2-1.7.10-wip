bae2fc54c7e595ee46cf9921d97e376e0a1c8f07
mengy007 <mengy007@gmail.com>
2015-07-28 11:22:37 -0700

    Initial Commit

---
+++ src/main/java/com/techmafia/mcmods/KinetiCraft2/tileentities/WoodenKineticEnergyCubeTileEntity.java
@@ -0,0 +1,14 @@
+package com.techmafia.mcmods.KinetiCraft2.tileentities;
+
+import net.minecraft.item.ItemStack;
+
+/**
+ * Created by Meng on 7/28/2015.
+ */
+public class WoodenKineticEnergyCubeTileEntity extends BaseKineticEnergyCubeTileEntity {
+    public WoodenKineticEnergyCubeTileEntity() {
+        super();
+
+        this.energyCores = new ItemStack[1];
+    }
+}
